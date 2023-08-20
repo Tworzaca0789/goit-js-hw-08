7a5dc04b4455e2192f669c73bf5ea199277c8cad
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-08-21 00:23:23 +0200

    creating photo gallery with SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryLi = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+  <a class = "gallery__link" href = "${original}">
+  <img class = "gallery__image" src = "${preview}"
+  alt = "${description}"
+   />
+  </a></li>`;
 
+const galleryListLi = galleryItems.map(el => galleryLi(el)).join('');
+gallery.innerHTML = galleryListLi;
+
+gallery.addEventListener('click', onClickCollectionGal);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+function onClickCollectionGal(e) {
+  e.preventDefault();
+}
 console.log(galleryItems);
