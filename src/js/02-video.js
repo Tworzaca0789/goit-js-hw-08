01f2df967da0eba186f9d15622296b484e6a1252
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-08-22 18:02:27 +0200

    creating a task with a video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const iframeVimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframeVimeoPlayer);
+
+const playTime = 1000;
+const keyStorage = 'videoplayer-current-time';
+
+const onPlay = keyTime => {
+  player.getEnded().then(end => {
+    if (end) {
+      localStorage.setItem(keyStorage, 0);
+    } else {
+      localStorage.setItem(keyStorage, Number(keyTime.seconds));
+    }
+  });
+};
+player
+  .setCurrentTime(localStorage.getItem(keyStorage))
+  .then(console.log('seconds = the actual time that the player seeked to'))
+  .catch(error =>
+    console.log('the time was less than 0 or greater than the video’s duration')
+  );
+player.on('timeupdate', throttle(onPlay, playTime));
