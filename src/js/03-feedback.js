40806fa14ad40939ea4d148a896ab2e293235457
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-08-30 21:16:31 +0200

    save the form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,6 @@ const { email, message } = form.elements;
 //tworze klucz do localStorage.getItem
 const localStorageKey = 'feedback-form-state';
 
-const formData = getSavedData;
 //dodawanie do localeStorage
 form.addEventListener('input', throttle(inputForm, updateStorageTime));
 function inputForm(e) {
@@ -16,31 +15,17 @@ function inputForm(e) {
   );
   localStorage.getItem(localStorageKey);
 }
-/*form.addEventListener(
-  'input',
-  throttle(() => {
-    console.log(`Input handler call every ${updateStorageTime}`);
-  }, updateStorageTime)
-);
-*/
 //pobieramy to co zapisalismy i dodajemy do formularza
+const formData = localStorage.getItem(localStorageKey);
 window.addEventListener('load', e => {
   // tutaj cała logika pobrania z localStorage i wrzucenia do formularza/ustawienia value
-  return JSON.parse(localStorage.getItem(localStorageKey));
-});
 
-function getSavedData() {
-  try {
-    return JSON.parse(localStorage.getItem(localStorageKey)) || {};
-  } catch (error) {
-    console.log(`${error.email}: ${error.message}`);
-  }
   if (formData) {
+    const parsedData = JSON.parse(formData);
     email.value = formData.email || '';
     message.value = formData.message || '';
   }
-  localStorage.removeItem(localStorageKey.reset());
-}
+});
 
 //usługa submit do czyszczenia localeStorage
 form.addEventListener('submit', e => {
